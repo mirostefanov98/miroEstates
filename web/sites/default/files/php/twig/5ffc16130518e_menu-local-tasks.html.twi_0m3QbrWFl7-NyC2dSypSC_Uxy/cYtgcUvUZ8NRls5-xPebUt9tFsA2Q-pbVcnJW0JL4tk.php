7a45f151414d9c25a7e1cf3f3f882d0e4f958ade
mirostefanov98 <45970592+mirostefanov98@users.noreply.github.com>
2021-01-11 11:18:07 +0200

    add newest estates block

---
+++ web/sites/default/files/php/twig/5ffc16130518e_menu-local-tasks.html.twi_0m3QbrWFl7-NyC2dSypSC_Uxy/cYtgcUvUZ8NRls5-xPebUt9tFsA2Q-pbVcnJW0JL4tk.php
@@ -0,0 +1,123 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/olivero/templates/navigation/menu-local-tasks.html.twig */
+class __TwigTemplate_a65edc393843c8ecf90f7de7c8d995527aad5e51de9acf1c4d26975afd69078d extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 16
+        echo "
+";
+        // line 17
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("olivero/tabs"), "html", null, true);
+        echo "
+
+";
+        // line 19
+        if (($context["primary"] ?? null)) {
+            // line 20
+            echo "  <h2 id=\"primary-tabs-title\" class=\"visually-hidden\">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(t("Primary tabs"));
+            echo "</h2>
+  <nav role=\"navigation\" class=\"tabs-wrapper\" aria-labelledby=\"primary-tabs-title\" data-drupal-nav-tabs>
+    <ul class=\"tabs tabs--primary\">";
+            // line 22
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["primary"] ?? null), 22, $this->source), "html", null, true);
+            echo "</ul>
+  </nav>
+";
+        }
+        // line 25
+        if (($context["secondary"] ?? null)) {
+            // line 26
+            echo "  <h2 id=\"secondary-tabs-title\" class=\"visually-hidden\">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(t("Secondary tabs"));
+            echo "</h2>
+  <nav role=\"navigation\" class=\"tabs-wrapper\" aria-labelledby=\"secondary-tabs-title\" data-drupal-nav-tabs>
+    <ul class=\"tabs tabs--secondary\">";
+            // line 28
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["secondary"] ?? null), 28, $this->source), "html", null, true);
+            echo "</ul>
+  </nav>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/olivero/templates/navigation/menu-local-tasks.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  69 => 28,  63 => 26,  61 => 25,  55 => 22,  49 => 20,  47 => 19,  42 => 17,  39 => 16,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/olivero/templates/navigation/menu-local-tasks.html.twig", "C:\\xampp\\htdocs\\course-project\\miroEstates\\web\\themes\\olivero\\templates\\navigation\\menu-local-tasks.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 19);
+        static $filters = array("escape" => 17, "t" => 20);
+        static $functions = array("attach_library" => 17);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape', 't'],
+                ['attach_library']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
